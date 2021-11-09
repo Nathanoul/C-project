90bcc6762e4dbded2dff4ad790b6b732f91a5092
Nathanoul <92688201+Nathanoul@users.noreply.github.com>
2021-11-09 23:53:25 +0300

    Update hw1.cpp

--- homework1/hw1.cpp
+++ homework1/hw1.cpp
@@ -9,21 +9,20 @@ int main()
 {
     ifstream in("in.txt"); 
 
-    float x_n = 0;
-    float y_n = 0;
-    in >> x_n >> y_n;
-
     float x_0 = 0;
     float y_0 = 0;
     in >> x_0 >> y_0;
 
-    float x_frst_l = x_0;
-    float y_frst_l = y_0;
-    float x_frst_r = x_0;
-    float y_frst_r = y_0;
+    float x_n = 0;
+    float y_n = 0;
 
-    float dist_l = 0;
-    float dist_r = 0;
+    float x_obtuse_l = x_0;
+    float y_obtuse_l = y_0;
+    float x_obtuse_r = x_0;
+    float y_obtuse_r = y_0;
+
+    float cos_l = 1;
+    float cos_r = 1;
 
     while(in)
     {
@@ -31,24 +30,24 @@ int main()
 
         if ( x_0 != 0 && y_0 != 0)
         {
-            if ( (dist_l < x_n*x_n*y_0*y_0 + x_0*x_0*y_n*y_n - 2*x_n*x_0*y_n*y_0) && (x_n*y_0 - y_n*x_0 >= 0) )
+            if ( (cos_l > (x_0*x_n + y_n*y_0)/sqrt((x_0*x_0 + y_0*y_0)*(x_n*x_n + y_n*y_n))) && (x_n*y_0 - y_n*x_0 <= 0) ) 
             {
-                dist_l = x_n*x_n*y_0*y_0 + x_0*x_0*y_n*y_n - 2*x_n*x_0*y_n*y_0;
-                x_frst_l = x_n;
-                y_frst_l = y_n;
+                cos_l = (x_0*x_n + y_n*y_0)/sqrt((x_0*x_0 + y_0*y_0)*(x_n*x_n + y_n*y_n));
+                x_obtuse_l = x_n;
+                y_obtuse_l = y_n;
             }
 
-            if ( (dist_r < x_n*x_n*y_0*y_0 + x_0*x_0*y_n*y_n - 2*x_n*x_0*y_n*y_0) && (x_n*y_0 - y_n*x_0 < 0) )
+            if ( (cos_r > (x_0*x_n + y_n*y_0)/sqrt((x_0*x_0 + y_0*y_0)*(x_n*x_n + y_n*y_n))) && (x_n*y_0 - y_n*x_0 >= 0) )
             {
-                dist_r = x_n*x_n*y_0*y_0 + x_0*x_0*y_n*y_n - 2*x_n*x_0*y_n*y_0;
-                x_frst_r = x_n;
-                y_frst_r = y_n;
+                cos_r = (x_0*x_n + y_n*y_0)/sqrt((x_0*x_0 + y_0*y_0)*(x_n*x_n + y_n*y_n));
+                x_obtuse_r = x_n;
+                y_obtuse_r = y_n;
             }
         } 
     }
      
-    cout << "rightmost: " << x_frst_r << " " << y_frst_r << std::endl;
-    cout << "leftmost: " << x_frst_l << " " << y_frst_l << std::endl;
+    cout << "rightmost: " << x_obtuse_r << " " << y_obtuse_r << std::endl;
+    cout << "leftmost: " << x_obtuse_l << " " << y_obtuse_l << std::endl;
     return 0;
 }
    
