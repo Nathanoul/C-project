4c1e4ddd0f5f1e489e300d5aaeee3ea4cbaeb6ee
Nathanoul <92688201+Nathanoul@users.noreply.github.com>
2021-11-15 11:06:18 +0300

    Update hw1.cpp

--- homework1/hw1.cpp
+++ homework1/hw1.cpp
@@ -30,14 +30,14 @@ int main()
 
         if ( x_n != 0 && y_n != 0)
         {
-            if ( (cos_l >= (x_0*x_n + y_n*y_0)/sqrt((x_0*x_0 + y_0*y_0)*(x_n*x_n + y_n*y_n))) && (x_n*y_0 - y_n*x_0 <= 0) ) 
+            if ( (cos_l > (x_0*x_n + y_n*y_0)/sqrt((x_0*x_0 + y_0*y_0)*(x_n*x_n + y_n*y_n))) && (x_n*y_0 - y_n*x_0 <= 0) || ( x_n*y_n == x_obtuse_l*y_obtuse_l ) ) 
             {
                 cos_l = (x_0*x_n + y_n*y_0)/sqrt((x_0*x_0 + y_0*y_0)*(x_n*x_n + y_n*y_n));
                 x_obtuse_l = x_n;
                 y_obtuse_l = y_n;
             }
 
-            if ( (cos_r >= (x_0*x_n + y_n*y_0)/sqrt((x_0*x_0 + y_0*y_0)*(x_n*x_n + y_n*y_n))) && (x_n*y_0 - y_n*x_0 > 0) )
+            if ( (cos_r > (x_0*x_n + y_n*y_0)/sqrt((x_0*x_0 + y_0*y_0)*(x_n*x_n + y_n*y_n))) && (x_n*y_0 - y_n*x_0 > 0) || ( x_n*y_n == x_obtuse_r*y_obtuse_r ) )
             {
                 cos_r = (x_0*x_n + y_n*y_0)/sqrt((x_0*x_0 + y_0*y_0)*(x_n*x_n + y_n*y_n));
                 x_obtuse_r = x_n;
