557093406e4cb11d655c71cb8bf803cac7d59f9b
Nathanoul <92688201+Nathanoul@users.noreply.github.com>
2021-12-22 17:06:07 +0300

    Update hw1.cpp

--- homework1/hw1.cpp
+++ homework1/hw1.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int main()
 {
     ifstream in("in.txt"); 
+    const double eps = 1e-15;
 
     double x_0 = 0;
     double y_0 = 0;
@@ -30,14 +31,14 @@ int main()
 
         if ( x_n*x_n + y_n*y_n != 0)
         {
-            if ( ( (cos_l > (x_0*x_n + y_n*y_0)/sqrt((x_0*x_0 + y_0*y_0)*(x_n*x_n + y_n*y_n))) || (x_obtuse_l*y_n == x_n*y_obtuse_l) ) && (x_n*y_0 - y_n*x_0 < 0) )
+            if ( ( (cos_l > (x_0*x_n + y_n*y_0)/sqrt((x_0*x_0 + y_0*y_0)*(x_n*x_n + y_n*y_n))) || (abs(x_obtuse_l*y_n - x_n*y_obtuse_l) < eps) ) && (x_n*y_0 - y_n*x_0 < 0) )
             {
                 cos_l = (x_0*x_n + y_n*y_0)/sqrt((x_0*x_0 + y_0*y_0)*(x_n*x_n + y_n*y_n));
                 x_obtuse_l = x_n;
                 y_obtuse_l = y_n;
             }
 
-            if ( ( (cos_r > (x_0*x_n + y_n*y_0)/sqrt((x_0*x_0 + y_0*y_0)*(x_n*x_n + y_n*y_n))) || (x_obtuse_r*y_n == x_n*y_obtuse_r) ) && (x_n*y_0 - y_n*x_0 >= 0) ) 
+            if ( ( (cos_r > (x_0*x_n + y_n*y_0)/sqrt((x_0*x_0 + y_0*y_0)*(x_n*x_n + y_n*y_n))) || (abs(x_obtuse_r*y_n - x_n*y_obtuse_r) < eps) ) && (x_n*y_0 - y_n*x_0 >= 0) ) 
             {
                 cos_r = (x_0*x_n + y_n*y_0)/sqrt((x_0*x_0 + y_0*y_0)*(x_n*x_n + y_n*y_n));
                 x_obtuse_r = x_n;
@@ -50,3 +51,4 @@ int main()
     cout << "Rightmost: " << x_obtuse_r << " " << y_obtuse_r << std::endl;
     return 0;
 }
+   
