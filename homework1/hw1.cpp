70f80bc91063dbc970d8ce7c380d1a939f25027a
Nathanoul <92688201+Nathanoul@users.noreply.github.com>
2021-12-23 12:28:22 +0300

    Update hw1.cpp

--- homework1/hw1.cpp
+++ homework1/hw1.cpp
@@ -35,14 +35,14 @@ int main()
             cos = (x_0*x_n + y_n*y_0)/sqrt((x_0*x_0 + y_0*y_0)*(x_n*x_n + y_n*y_n));
             if ( ( ((abs(cos_l - cos) > eps) && (cos_l > cos)) || (abs(x_obtuse_l*y_n - x_n*y_obtuse_l) < eps) ) && (x_n*y_0 - y_n*x_0 < 0) )
             {
-                cos_l = (x_0*x_n + y_n*y_0)/sqrt((x_0*x_0 + y_0*y_0)*(x_n*x_n + y_n*y_n));
+                cos_l = cos;
                 x_obtuse_l = x_n;
                 y_obtuse_l = y_n;
             }
 
             if ( ( ((abs(cos_r - cos) > eps) && (cos_r > cos)) || (abs(x_obtuse_r*y_n - x_n*y_obtuse_r) < eps) ) && (x_n*y_0 - y_n*x_0 >= 0) ) 
             {
-                cos_r = (x_0*x_n + y_n*y_0)/sqrt((x_0*x_0 + y_0*y_0)*(x_n*x_n + y_n*y_n));
+                cos_r = cos;
                 x_obtuse_r = x_n;
                 y_obtuse_r = y_n;
             }
